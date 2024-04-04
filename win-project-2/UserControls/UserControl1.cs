dd368508100898a58be5cfcf027b52fca300e0ca
minhnhat1206 <minhnhatnhc@gmail.com>
2024-04-04 18:19:33 +0700

    tạo UC Thợ

---
+++ win-project-2/UserControls/UserControl1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace win_project_2.UserControls
+{
+    public partial class UCTho : UserControl
+    {
+        public UCTho()
+        {
+            InitializeComponent();
+        }
+
+        private void lb_job_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
