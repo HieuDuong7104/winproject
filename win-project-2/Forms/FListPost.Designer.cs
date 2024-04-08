40a4c2ea51a1105b8e5b0da217af65467623576b
CaoThang123 <157550894+CaoThang123@users.noreply.github.com>
2024-04-08 17:46:15 +0700

    them tim kiem

--- win-project-2/Forms/FListPost.Designer.cs
+++ win-project-2/Forms/FListPost.Designer.cs
@@ -28,41 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FListPost));
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             this.ucShowPost1 = new win_project_2.UserControls.UCShowPost();
-            this.ucShowPost2 = new win_project_2.UserControls.UCShowPost();
+            this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2ImageButton1 = new Guna.UI2.WinForms.Guna2ImageButton();
             this.guna2Panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // guna2Panel1
             // 
-            this.guna2Panel1.Controls.Add(this.ucShowPost2);
             this.guna2Panel1.Controls.Add(this.ucShowPost1);
-            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 87);
             this.guna2Panel1.Name = "guna2Panel1";
-            this.guna2Panel1.Size = new System.Drawing.Size(721, 702);
+            this.guna2Panel1.Size = new System.Drawing.Size(721, 615);
             this.guna2Panel1.TabIndex = 0;
             // 
             // ucShowPost1
             // 
-            this.ucShowPost1.Location = new System.Drawing.Point(22, 12);
+            this.ucShowPost1.Location = new System.Drawing.Point(23, 15);
             this.ucShowPost1.Name = "ucShowPost1";
             this.ucShowPost1.Size = new System.Drawing.Size(676, 231);
             this.ucShowPost1.TabIndex = 0;
             // 
-            // ucShowPost2
+            // guna2TextBox1
             // 
-            this.ucShowPost2.Location = new System.Drawing.Point(12, 12);
-            this.ucShowPost2.Name = "ucShowPost2";
-            this.ucShowPost2.Size = new System.Drawing.Size(697, 231);
-            this.ucShowPost2.TabIndex = 1;
+            this.guna2TextBox1.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.guna2TextBox1.BorderRadius = 24;
+            this.guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextBox1.DefaultText = "search";
+            this.guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.FillColor = System.Drawing.SystemColors.Control;
+            this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2TextBox1.ForeColor = System.Drawing.Color.Black;
+            this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.Location = new System.Drawing.Point(59, 26);
+            this.guna2TextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.guna2TextBox1.Name = "guna2TextBox1";
+            this.guna2TextBox1.PasswordChar = '\0';
+            this.guna2TextBox1.PlaceholderText = "";
+            this.guna2TextBox1.SelectedText = "";
+            this.guna2TextBox1.Size = new System.Drawing.Size(464, 37);
+            this.guna2TextBox1.TabIndex = 1;
+            // 
+            // guna2ImageButton1
+            // 
+            this.guna2ImageButton1.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButton1.HoverState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButton1.Image = ((System.Drawing.Image)(resources.GetObject("guna2ImageButton1.Image")));
+            this.guna2ImageButton1.ImageOffset = new System.Drawing.Point(0, 0);
+            this.guna2ImageButton1.ImageRotate = 0F;
+            this.guna2ImageButton1.ImageSize = new System.Drawing.Size(40, 40);
+            this.guna2ImageButton1.Location = new System.Drawing.Point(530, 26);
+            this.guna2ImageButton1.Name = "guna2ImageButton1";
+            this.guna2ImageButton1.PressedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButton1.Size = new System.Drawing.Size(77, 45);
+            this.guna2ImageButton1.TabIndex = 2;
             // 
             // FListPost
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(721, 702);
+            this.Controls.Add(this.guna2ImageButton1);
+            this.Controls.Add(this.guna2TextBox1);
             this.Controls.Add(this.guna2Panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FListPost";
@@ -76,6 +109,7 @@
 
         private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
         private UserControls.UCShowPost ucShowPost1;
-        private UserControls.UCShowPost ucShowPost2;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private Guna.UI2.WinForms.Guna2ImageButton guna2ImageButton1;
     }
 }
